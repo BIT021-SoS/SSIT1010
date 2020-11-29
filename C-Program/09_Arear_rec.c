67ecccd23fbb844d7ea65cb8f16a21adf9a376b1
Ruvel Katrodiya <73926150+BIT021-SoS@users.noreply.github.com>
2020-11-29 10:01:17 +0530

    Add files via upload

---
+++ C-Program/09_Arear_rec.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main(){
+    float length,width;
+    printf("Enter size of Length: ");
+    scanf("%f",&length);
+    printf("Enter size of Width: ");
+    scanf("%f",&width);
+    printf("your area of Ractangle(%f x %f) is %f Meter Square\n ",length,width,(float)(length*width));
+
+    return 0;
+}   
